afc4e0b4c98b27e9032fb5828c2a9e54693a6114
WatchMenDoor <13218857038@163.com>
2020-10-06 10:14:28 +0900

    添加负载均衡设置

--- springbootdubbo_provider/src/main/java/com/bjsxt/springbootdubbo_provider/service/impl/DemoDubboServiceImpl.java
+++ springbootdubbo_provider/src/main/java/com/bjsxt/springbootdubbo_provider/service/impl/DemoDubboServiceImpl.java
@@ -3,10 +3,11 @@ package com.bjsxt.springbootdubbo_provider.service.impl;
 import com.bjsxt.dubbo.service.DemoDubboService;
 import org.apache.dubbo.config.annotation.Service;
 
-@Service
+@Service(loadbalance = "roundroubin" ,weight = 3)
 public class DemoDubboServiceImpl implements DemoDubboService {
     @Override
     public String showMsg(String str) {
+        System.out.println("8889===========");
         return "Hello Dubbo "+str;
     }
 }
